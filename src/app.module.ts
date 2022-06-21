3b2c370f6029a374920760a372158dbea2267ac2
Macsoftechnologies <macsoftechnologies@gmail.com>
2022-06-21 11:34:14 +0530

    created a project with few modules

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
+import { ProductsModule } from './products/products.module';
+import { ServicesModule } from './services/services.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://macsof:macsof@nextlevelcarwash.yjs3i.mongodb.net/eCommerce?retryWrites=true&w=majority'),
+  UsersModule, AdminModule, ProductsModule, ServicesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
